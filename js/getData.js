fbf1e02d5fd3a81ab2a6da78922ecae0349fdc32
mlmedina92 <lm30540@gmail.com>
2022-05-17 18:37:31 -0300

    correccion del fetch

--- js/getData.js
+++ js/getData.js
@@ -2,7 +2,7 @@
 // le paso por parametro el nombre del json:
 const getData = async (jsonName) => {
     try {
-        const response = await fetch("../data/"+jsonName+".json");// uso el fetch y linkeo mi ruta relativa - AWAY : le digo q espere una rta de esa consulta q se va a hacer medinate el fecth a esa ruta relativa.Guardo lo q obtengo de la consulta en variable response. ESPERA HASTA OBTENER UN DATO O OBTENER UN ERROR donde se va a capturar  x el catch  - el await espera esa consulta y una vez q lo obtiene lo almacena en response.
+        const response = await fetch("./data/"+jsonName+".json");// uso el fetch y linkeo mi ruta relativa - AWAY : le digo q espere una rta de esa consulta q se va a hacer medinate el fecth a esa ruta relativa.Guardo lo q obtengo de la consulta en variable response. ESPERA HASTA OBTENER UN DATO O OBTENER UN ERROR donde se va a capturar  x el catch  - el await espera esa consulta y una vez q lo obtiene lo almacena en response.
         const data = await response.json();  // response esta almacenada en formato json(stok.json ·"color":"rojo"). tengo q parsear esos datos:  parsear a un formato de array de objetos con metodo .JSON --- response no es palabra reservada. Response sigue siendo una promesa entonces le agrego el AWAY. lo guardo en const data donde se van a almacenar el array de objetos: los productos.
         return data; //al invocar esta funcion obtengo el array de objetos productos con sus prop.
     } catch (error) {
